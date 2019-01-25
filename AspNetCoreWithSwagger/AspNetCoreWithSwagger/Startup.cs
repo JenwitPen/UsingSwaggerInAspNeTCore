dbd3cc27fc5d5c0c840179ee2e1507c1a027404b
Jenwit <jenwit_penjamrat@hotmail.com>
2019-01-25 11:15:31 +0700

    Set up Swagger

--- AspNetCoreWithSwagger/AspNetCoreWithSwagger/Startup.cs
+++ AspNetCoreWithSwagger/AspNetCoreWithSwagger/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace AspNetCoreWithSwagger
 {
@@ -26,19 +27,38 @@ namespace AspNetCoreWithSwagger
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1.0", new Info
+                {
+                    Title = "Swagger With Asp.net core",
+                    Version = "v1",
+                    Contact = new Contact()
+                    {
+                        Name = "Jenwit Penjamrat",
+                        Email = "jenwit_penjamrat@hotmail.com"
+                    }
+                });
+            });
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
+
                 app.UseDeveloperExceptionPage();
             }
             else
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "My Demo API (V 1.0) Swagger With Asp.net core");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
